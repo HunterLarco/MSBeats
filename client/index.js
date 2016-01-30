121b8c7d2002132b468b0e64353e6e552d2671ea
Jeroen Ransijn <jssrdesign@gmail.com>
2016-01-30 10:57:56 -0800

    Tabs

--- client/index.js
+++ client/index.js
@@ -1,3 +1,4 @@
+// eslint-disable
 var express = require('express');
 var config = require('config');
 var app = express();
@@ -11,16 +12,16 @@ app.set('view engine', 'ejs');
 
 // Make the scripts avaiable to us on every call
 app.use(function (req, res, next) {
-  // Populate our locals with the config file
-  res.locals.scripts = config.get('scripts');
-  res.locals.stylesheets = config.get('stylesheets');
-  next();
+	// Populate our locals with the config file
+	res.locals.scripts = config.get('scripts');
+	res.locals.stylesheets = config.get('stylesheets');
+	next();
 });
 
 app.get('/', function (req, res) {
-  res.render('page/index.ejs');
+	res.render('page/index.ejs');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
